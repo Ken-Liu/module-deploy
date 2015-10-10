89a208dc758d30f74c3ed51f839300e4542185f9
liukunmcu <lkmcudevelope@gmail.com>
2015-10-10 22:44:56 +0800

    Changed docker image

--- templates/linux/start.sh
+++ templates/linux/start.sh
@@ -32,7 +32,7 @@ if [ "$USE_LOCAL_MONGO" == "1" ]; then
     --hostname="$HOSTNAME-$APPNAME" \
     --env=MONGO_URL=mongodb://mongodb:27017/$APPNAME \
     --name=$APPNAME \
-    maodou/microduino
+    liukunmcu/microduino:test
 else
   docker run \
     -d \
@@ -44,7 +44,7 @@ else
     --hostname="$HOSTNAME-$APPNAME" \
     --env=MONGO_URL=$MONGO_URL_COMPOSE \
     --name=$APPNAME \
-    maodou/microduino
+    liukunmcu/microduino:test
 
 
 
